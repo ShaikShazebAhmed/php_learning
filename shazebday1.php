01d8091dae85476ddc9e6cc0b5bfc3428eae603d
ShaikShazebAhmed <69285360+ShaikShazebAhmed@users.noreply.github.com>
2024-04-16 10:12:33 +0530

    Create shazebday1.php

---
+++ shazebday1.php
@@ -0,0 +1,21 @@
+<?php
+
+    $abc = 123;
+    $str="This is my first php file";
+    echo "<h1>" .$abc. "</h1>";
+
+    echo "<h1> $str </h1>";
+    
+    $num = 52.2565;
+
+    $NUM = 855575;
+
+    $str = "dlkfhfilf988248627%^&";
+
+    echo "<h5>" .$num. "</h5>";
+
+    echo "<h2>" .$NUM. "</h2>"; 
+
+    echo "<h2>" .$str. "</h2>";
+
+?>
